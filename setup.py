2b54c4fd7808eb1a567a987d0538bf9d47a2da7a
yash16jr <yash16jr@gmail.com>
2023-05-28 21:03:46 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+	    name = 'Credit_card_fraud',
+	    version = '0.0.1',
+	    author = 'Yash Kumar Singh',
+	    author_email =  'yash16jr@gmail.com',
+	    install_requires = ['pandas' , 'numpy' , 'sklearn'],
+	    packages = find_packages()
+
+
+)
